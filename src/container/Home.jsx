974916de6bcd88518cac1757b49db9d0841f79c4
iammurad <murad.theynalli@gmail.com>
2022-12-26 00:16:39 +0100

    sidebar completed

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -12,8 +12,11 @@ const Home = () => {
   
   const [toggleSidebar, setToggleSidebar] = useState(false)
   const [user, setUser] = useState(null)
-  const userInfo = localStorage.getItem('user') != undefined ? JSON.parse(localStorage.getItem('user')) :  localStorage.clear()
+  const scrollRef = useRef(null);
 
+  const userInfo = localStorage.getItem('user') != undefined ? JSON.parse(localStorage.getItem('user')) :  localStorage.clear()
+  
+  
   useEffect(() => {
     const query = userQuery(userInfo?.sub);
     client.fetch(query).then((data)=>{
@@ -21,6 +24,11 @@ const Home = () => {
       setUser(data[0]);
     })
   }, [])
+
+  useEffect(() => {
+    scrollRef.current.scrollTo(0,0)
+  }, [])
+  
   
 
   return (
@@ -29,6 +37,7 @@ const Home = () => {
         <Sidebar user={ user && user } closeToggle={setToggleSidebar} />
       </div>
       <div className="flex md:hidden flex-row">
+          <div className="p-2 w-full flex flex-row justify-between items-center shadow-md">
         <HiMenu fontSize={40} className="cursor-pointer" onClick={()=>setToggleSidebar(true)} />
         <Link to="/">
         <img src={logo} alt="logo" className="w-28"/>
@@ -36,6 +45,7 @@ const Home = () => {
         <Link to={`user-profile/${user?._id}`}>
         <img src={user?.image} alt="Profile" className="w-28"/>
         </Link>
+          </div>
         {
           toggleSidebar &&(
             <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
@@ -46,6 +56,14 @@ const Home = () => {
             </div>
           )}
       </div> 
+
+            <div className="pb-2 flex-1 h-screen overflow-y-scroll " ref={scrollRef}>
+                <Routes>
+                  <Route path="/user-profile/:userId" element={<UserProfile/>}/>
+                  <Route path="/*" element={<Pins user={user&&user}/>}/>
+                </Routes>
+            </div>
+
     </div>
   );
 };
