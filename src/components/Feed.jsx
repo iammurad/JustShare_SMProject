3cd7baa763531a2d7e10ff5e4e671ca5a800fdf9
iammurad <murad.theynalli@gmail.com>
2022-12-28 01:16:38 +0100

    added search functionality

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,15 +1,34 @@
-import React, {useState,useEffect} from 'react'
-import { useParams } from 'react-router-dom'
-import client from '../client'
-import MasonryLayout from './MasonryLayout'
-import Spinner from './Spinner'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import client from "../client";
+import { feedQuery, searchQuery } from "../utils/data";
+import MasonryLayout from "./MasonryLayout";
+import Spinner from "./Spinner";
 const Feed = () => {
-  const [loading, setloading] = useState(true)
+  const [loading, setLoading] = useState(false);
+  const [pins, setPins] = useState(null);
 
-  if(loading) return <Spinner message = "We are adding new ideas to your feed!" />
-  return (
-    <div>Feed</div>
-  )
-}
+  const { categoryId } = useParams();
 
-export default Feed
+  useEffect(() => {
+    setLoading(true);
+    if (categoryId) {
+      const query = searchQuery(categoryId);
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      client.fetch(feedQuery).then((data)=>{
+        setPins(data);
+        setLoading(false);
+      })
+    }
+  }, [categoryId]);
+
+  if (loading)
+    return <Spinner message="We are adding new ideas to your feed!" />;
+  return <div>Feed</div>;
+};
+
+export default Feed;
